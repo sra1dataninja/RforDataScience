488fb7d80420aed393d4e6f242b2f846c1fc08e3
sra1dataninja <38573487+sra1dataninja@users.noreply.github.com>
2018-04-22 22:35:23 -0400

    Add files via upload

--- Chapter 3. Data visualization.R
+++ Chapter 3. Data visualization.R
@@ -3,6 +3,7 @@ setwd("C://Users//sparital//Documents//R Workspace")
 getwd()
 
 library(tidyverse)
+library(maps)
 
 data(mpg)
 summary(mpg)
@@ -170,3 +171,110 @@ base2 + geom_point(aes(colour = drv)) + geom_smooth(aes(colour = drv), se=FALSE)
 base2 + geom_point(aes(colour = drv)) + geom_smooth(se=FALSE)
 base2 + geom_point(aes(colour = drv)) + geom_smooth(aes(linetype=drv), se=FALSE)
 base2 + geom_point(size = 4, colour = "white") + geom_point(aes(colour = drv))
+
+
+######################3.7 Statistical transformations#################################
+
+## Diamond data set is to be used for this section of the chapter
+
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
+
+ggplot(data= diamonds) +
+  geom_bar(mapping = aes(x=cut, color = clarity))
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, y = freq), stat = "identity")
+
+#########################3.8 Position adjustments ####################################
+ggplot(data = diamonds) +
+geom_bar(mapping = aes(x= cut, fill = cut))
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x= cut, color = cut))
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x= cut, fill = clarity))
+
+
+ggplot(data = diamonds, mapping = aes(x=cut, fill = clarity)) +
+  geom_bar(alpha = 1/5, position = "identity")
+
+
+ggplot(data = diamonds, mapping = aes(x=cut, colour = clarity)) +
+  geom_bar(fill = NA, position = "identity")
+
+##########to make proportions across the graph use this one below
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill= clarity), position = "fill")
+
+## to place one next to one another
+
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill= clarity), position = "dodge")
+
+## jitter concept
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy), position = "jitter")
+
+#### 3.8 Exercise #################
+### What is the problem with this code
+
+ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) + 
+  geom_point()
+
+ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) +
+  geom_point( position = "jitter", alpha = 0.5) +
+  geom_smooth(method = 'lm')
+
+### What???s the default position adjustment for geom_boxplot()? 
+##Create a visualisation of the mpg dataset that demonstrates it.
+base <- ggplot(data = mpg, mapping = aes(x = drv, y = cty, fill = as.factor(cyl)))
+
+base + geom_boxplot()
+
+base + geom_boxplot(position = "dodge")
+
+base + geom_boxplot(position = "identity")
+
+base + geom_boxplot(position = "jitter")
+
+
+
+#############3.9 Coordinate systems ####################
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x= class , y= hwy))
+
+## Flip this boxplot using coor flip function
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x= class , y= hwy)) +
+  coord_flip()
+
+usa <- map_data("usa")
+
+ggplot(usa, aes(long, lat, group = group)) +
+  geom_polygon(fill = "orange", color = "black")
+
+
+
+ggplot(usa, aes(long, lat, group = group)) +
+  geom_polygon(fill = "orange", color = "black") +
+  coord_quickmap()
+
+bar <- ggplot(data = diamonds) + 
+  geom_bar(
+    mapping = aes(x = cut, fill = cut), 
+    show.legend = FALSE,
+    width = 1
+  ) + 
+  theme(aspect.ratio = 1) +
+  labs(x = NULL, y = NULL)
+
+bar + coord_flip()
+bar + coord_polar()
